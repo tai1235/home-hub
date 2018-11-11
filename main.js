b5d70e2f5ac368b96eca0981d9f8ecca509a81f3
tainv <tai12351235@gmail.com>
2018-11-11 16:02:18 +0700

    Initial commit

--- main.js
+++ main.js
@@ -2,12 +2,3 @@ const fs = require('fs');
 const Log = require('./lib/system-log');
 
 const log = new Log(fs.createWriteStream('/tmp/log.txt'));
-// const log = new Log();
-
-log.fatal('fatal condition');
-log.critical('critical condition');
-log.error('error condition');
-log.warning('warning condition');
-log.info('sending email');
-log.debug('preparing email');
-
