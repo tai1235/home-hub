583ddeaeed6e597eed58cd52774a1c055aa551b0
duongductai1408 <38109691+duongductai1408@users.noreply.github.com>
2019-03-14 19:34:57 +0700

    Taibeo Update

--- coordinator/zigbee-gateway.js
+++ coordinator/zigbee-gateway.js
@@ -21,7 +21,7 @@ const SubscribeTopics = {
 
 // TODO Liet ke cac command
 const ZigbeeCommand = {
-    CreateNetwork: 'pluin network-creator start',
+    CreateNetwork: 'plugin network-creator start',
     PermitJoin: '',
     PluginDeviceTable: 'plugin device-table',
     OnOff: 'zcl on-off',
@@ -30,14 +30,105 @@ const ZigbeeCommand = {
 const PublishTopic = 'command';
 
 // TODO Taibeo Liet ke cac clusterId
-const ClusterID = {
-    BASIC: '0x0000',
-    ONOFF: '0x0006'
-};
+const ZigbeeCluster = {
+    BASIC: {
+        ID: '0x0000',
+        Attribute: {
+            ZCL_VERSION_ATTRIBUTE_ID: {
+                ID: '0002',
+                type: '20'
+            },
+            ZCL_MANUFACTURER_NAME_ATTRIBUTE_ID: {
+                ID: '0004',
+                type: '42'
+            },
+            ZCL_MODEL_IDENTIFIER_ATTRIBUTE_ID: {
+                ID: '0005',
+                type: '42'
+            },
+            ZCL_POWER_SOURCE_ATTRIBUTE_ID: {
+                ID: '0007',
+                type: '30'
+            },
+            ZCL_BASIC_CLUSTER_CLUSTER_REVISION_SERVER_ATTRIBUTE_ID: {
+                ID: 'FFFD',
+                type: '21'
+            }
+        }
+    },
+    ONOFF: {
+        ID: '0x0006',
+        Attribute: {
+            ZCL_ON_OFF_ATTRIBUTE_ID: {
+                ID: '0000',
+                type: '10'
+            },
+            ZCL_ON_OFF_CLUSTER_CLUSTER_REVISION_SERVER_ATTRIBUTE_ID: {
+                ID: 'FFFD',
+                type: '21'
+            }
+        },
+        Command: {
+            On: '01',
+            Off: '00',
+            Toggle: '02',
+            OffWithEffect: '40',
+            OnWithTimeOff: '42'
+        }
+    },
+    IDENTIFY: {
+        ID: '0x0003',
+        Attribute: {
+            ZCL_IDENTIFY_TIME_ATTRIBUTE_ID:{
+                ID: '0000',
+                type: '21'
+            },
+            ZCL_COMMISSION_STATE_ATTRIBUTE_ID: {
+                ID: '0001',
+                type: '18'
+            },
+            ZCL_IDENTIFY_CLUSTER_CLUSTER_REVISION_SERVER_ATTRIBUTE_ID: {
+                ID: 'FFFD',
+                type: '21'
+            }
+        },
+        Command: {
+            Identify: '00',
+            IdentifyQuery: '01'
+        }
+    },
+    LEVELCONTROL: {
+        ID: '0x0008',
+        Attribute: {
+            ZCL_CURRENT_LEVEL_ATTRIBUTE_ID: {
+                ID: '0000',
+                type: '20'
+            },
+            ZCL_LEVEL_CONTROL_REMAINING_TIME_ATTRIBUTE_ID: {
+                ID: '0001',
+                type: '21'
+            },
+            ZCL_ON_LEVEL_ATTRIBUTE_ID: {
+                ID: '0011',
+                type: '20'
+            },
+            ZCL_LEVEL_CONTROL_CLUSTER_CLUSTER_REVISION_SERVER_ATTRIBUTE_ID: {
+                ID: 'FFFD',
+                type: '21'
+            }
+        },
+        Command: {
+            MoveToLevel: '00',
+            Move: '01',
+            Step: '02',
+            Stop: '03',
+            MoveToLevelWithOnOff: '04',
+            MoveWithOnOff: '05',
+            StepWithOnOff: '06',
+            StopWithOnOff: '07'
+        }
 
-// TODO Taibeo Liet ke cac attribute
-const Attribute = {
-
+    }
 };
 
 // Container for all methods
@@ -84,12 +175,7 @@ class ZigbeeGateway extends EventEmitter {
                         // TODO Parse number of endpoint (fixed / from message)
                         // TODO Parse device information ()
                         // TODO Pass device's data to the event emitter
-                        let params = {
-                            type: messageData.deviceType,
-                            eui64: messageData.deviceEndpoint.eui64,
-                            endpoint: messageData.deviceEndpoint.endpoint
-                        };
-                        this.emit('device-joined', params);
+                        this.emit('device-joined', message);
                     }
                 } break;
                 case SubscribeTopics.DeviceLeft: {
@@ -149,13 +235,27 @@ class ZigbeeGateway extends EventEmitter {
         let attribute = {};
         switch (clusterId) {
             // TODO Taibeo Switch theo cac case cuar Attribute duoc liet ke o tren
-            case '0x0000': {
+            case ZigbeeCluster.ONOFF.ID: {
+                switch (commandData.substring(2, 5)) {
+                    case ZigbeeCluster.ONOFF.Attribute.ZCL_ON_OFF_ATTRIBUTE_ID.ID: {
+                        if (commandData.substring(6, 7) !== ZigbeeCluster.ONOFF.Attribute.ZCL_ON_OFF_ATTRIBUTE_ID.type) {
+                            return false;
+                        } else {
+                            attribute.id = ZigbeeCluster.ONOFF.Attribute.ZCL_ON_OFF_ATTRIBUTE_ID.ID;
+                            attribute.dataType = ZigbeeCluster.ONOFF.Attribute.ZCL_ON_OFF_ATTRIBUTE_ID.type;
+                            attribute.dataValue = commandData.substring(7, 8) === '01' ? true : false;
+                        }
+                    } break;
+                    case ZigbeeCluster.ONOFF.Attribute.ZCL_ON_OFF_CLUSTER_CLUSTER_REVISION_SERVER_ATTRIBUTE_ID.ID {
 
+                    } break;
+                    default: {
+
+                    } break;
+                }
             } break;
-            case '0x0006': {
-                attribute.id = commandData.substring(2, 5);
-                attribute.dataType = commandData.substring(6, 7);
-                attribute.dataValue = commandData.substring(7, 8);
+            case ZigbeeCluster.BASIC.ID: {
+                
             } break;
         }
         return attribute;
