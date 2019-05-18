ede677ffab176dccde65d021cce47f3ed094a5f4
tai1235 <tlnfa1235>
2019-05-18 22:42:40 +0700

    Fix Dimmer control command

--- coordinator/zigbee/zigbee-gateway.js
+++ coordinator/zigbee/zigbee-gateway.js
@@ -151,7 +151,7 @@ class ZigbeeGateway extends EventEmitter {
                  * {Number} params.time - Transition time
                  */
                 payload.commands.push({
-                    command: ZigbeeCommand.LevelControl.MoveToLevel + ' ' + params.level + ' ' + params.time,
+                    command: ZigbeeCommand.LevelControl.MoveToLevel + ' ' + params.level + ' ' + params.time + ' 0 0',
                     postDelayMs: postTimeDelay
                 });
                 payload.commands.push({
